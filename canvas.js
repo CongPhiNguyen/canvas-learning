ce24a47d25b040c1af1424d8bce6344056c63865
CongPhiNguyen <congphinguyen312@gmail.com>
2022-08-04 03:02:00 +0700

    interactive canvas

--- canvas.js
+++ canvas.js
@@ -1,7 +1,7 @@
 let canvas = document.querySelector("canvas");
 // canvas.width = window.innerWidth - 60;
 // canvas.height = window.innerHeight - 6;
-canvas.width = 300;
+canvas.width = 600;
 canvas.height = 400;
 
 window.addEventListener("resize", function () {
@@ -29,48 +29,71 @@ let c = canvas.getContext("2d");
 // c.arc(100, 45, 199, (Math.PI * 55) / 360, (Math.PI * 150) / 360, false);
 // c.stroke();
 
-function Circle(x, y, radius, color, xVelocity, yVelocity) {
+let mouseObject = {
+  x: 0,
+  y: 0,
+};
+
+window.addEventListener("mousemove", (e) => {
+  [mouseObject.x, mouseObject.y] = [e.clientX, e.clientY];
+  console.log(mouseObject);
+});
+
+function Circle(x, y, radius, color, backgroundColor, xVelocity, yVelocity) {
   this.x = x;
   this.y = y;
 
   this.radius = radius;
+  this.currentRadius = radius;
+  this.minRadius = randomBetween(1, radius);
 
   this.xVelocity = xVelocity;
   this.yVelocity = yVelocity;
 
-  this.xDirection = 1 * xVelocity;
-  this.yDirection = 1 * yVelocity;
+  this.xDirection = Math.random() > 0.5 ? 1 : -1;
+  this.yDirection = Math.random() > 0.5 ? 1 : -1;
 
   this.color = color;
-
+  this.backgroundColor = backgroundColor;
   this.draw = () => {
     c.beginPath();
 
-    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+    c.arc(this.x, this.y, this.currentRadius, 0, Math.PI * 2, false);
 
     c.strokeStyle = this.color;
-
     c.stroke();
+    c.fillStyle = this.backgroundColor;
+    c.fill();
   };
 
   this.update = () => {
     if (this.x <= this.radius) {
-      this.xDirection = 1 * this.xVelocity;
-      this.xVelocity = Math.round(Math.random() * 5);
+      this.xDirection = 1;
+      this.xVelocity = Math.round(Math.random() * 16) / 12;
     } else if (this.x >= canvas.width - this.radius) {
-      this.xDirection = -1 * this.xVelocity;
-      this.xVelocity = Math.round(Math.random() * 5);
+      this.xDirection = -1;
+      this.xVelocity = Math.round(Math.random() * 16) / 12;
     }
     if (this.y <= this.radius) {
-      this.yDirection = 1 * this.yVelocity;
-      this.yVelocity = Math.round(Math.random() * 5);
+      this.yDirection = 1;
+      this.yVelocity = Math.round(Math.random() * 16) / 12;
     } else if (this.y >= canvas.height - this.radius) {
-      this.yDirection = -1 * this.yVelocity;
-      this.yVelocity = Math.round(Math.random() * 5);
+      this.yDirection = -1;
+      this.yVelocity = Math.round(Math.random() * 16) / 12;
     }
 
-    this.x += this.xDirection;
-    this.y += this.yDirection;
+    this.x += this.xDirection * this.xVelocity;
+    this.y += this.yDirection * this.yVelocity;
+
+    if (
+      Math.abs(this.x - mouseObject.x) < 50 &&
+      Math.abs(this.y - mouseObject.y) < 50 &&
+      this.currentRadius < 40
+    ) {
+      this.currentRadius++;
+    } else if (this.currentRadius > this.minRadius) {
+      this.currentRadius--;
+    }
 
     this.draw();
   };
@@ -83,28 +106,31 @@ const randomBetween = (a, b) => {
 };
 
 const makeCircleList = () => {
-  const radius = randomBetween(10, 30);
+  const radius = randomBetween(4, 8);
   const randomX = randomBetween(radius, canvas.width - radius);
   const randomY = randomBetween(radius, canvas.height - radius);
   const randomColor = `rgb(${randomBetween(0, 255)},
   ${randomBetween(0, 255)},
   ${randomBetween(0, 255)})`;
-
-  const randomXVelocity = randomBetween(1, 10);
-  const randomYVelocity = randomBetween(1, 10);
+  const randomBackgroundColor = `rgb(${randomBetween(0, 255)},
+  ${randomBetween(0, 255)},
+  ${randomBetween(0, 255)})`;
+  const randomXVelocity = randomBetween(1, 3);
+  const randomYVelocity = randomBetween(1, 3);
   circleList.push(
     new Circle(
       randomX,
       randomY,
       radius,
       randomColor,
+      randomBackgroundColor,
       randomXVelocity,
       randomYVelocity
     )
   );
 };
 
-for (let i = 0; i < 100; i++) {
+for (let i = 0; i < 400; i++) {
   makeCircleList();
 }
 
