e02c53189e755daeb3a6d143c49b1814636f4ee7
CongPhiNguyen <congphinguyen312@gmail.com>
2022-08-12 05:48:57 +0700

    finish sinewave

--- SineWave/canvas.js
+++ SineWave/canvas.js
@@ -1,152 +1,68 @@
-function randomIntFromRange(min, max) {
-  return Math.floor(Math.random() * (max - min + 1) + min);
-}
-
-function randomColor(colors) {
-  return colors[Math.floor(Math.random() * colors.length)];
-}
-
-function distance(x1, y1, x2, y2) {
-  const xDist = x2 - x1;
-  const yDist = y2 - y1;
-
-  return Math.sqrt(Math.pow(xDist, 2) + Math.pow(yDist, 2));
-}
-
 const canvas = document.querySelector("canvas");
 const c = canvas.getContext("2d");
+
+const gui = new dat.GUI();
 
 canvas.width = innerWidth - 16;
 canvas.height = innerHeight - 16;
 
-const mouse = {
-  x: innerWidth / 2,
-  y: innerHeight / 2,
+const wave = {
+  y: canvas.height / 2,
+  length: 0.01,
+  amplitude: 100,
+  frequency: 0.01,
 };
 
-const colors = ["#2185C5", "#7ECEFD", "#FF16E5", "#FF7F66"];
+const colorControl = {
+  color: "#00ffff",
+  r: 255,
+  g: 255,
+  b: 0,
+};
+const waveFolder = gui.addFolder("wave");
+waveFolder.add(wave, "y", 0, canvas.height);
+waveFolder.add(wave, "length", -0.01, 0.01);
+waveFolder.add(wave, "amplitude", -300, 300);
+waveFolder.add(wave, "frequency", -0.01, 1);
+waveFolder.open();
+// gui.addColor(colorControl, "color", "#00ffff");
+const colorFolder = gui.addFolder("color");
+colorFolder.add(colorControl, "r", 0, 255);
+colorFolder.add(colorControl, "g", 0, 255);
+colorFolder.add(colorControl, "b", 0, 255);
+colorFolder.open();
 
-// Event Listeners
-addEventListener("mousemove", (event) => {
-  mouse.x = event.clientX;
-  mouse.y = event.clientY;
-});
+function init() {}
 
-addEventListener("resize", () => {
-  canvas.width = innerWidth;
-  canvas.height = innerHeight;
-
-  init();
-});
-
-function getDistance(x1, y1, x2, y2) {
-  return Math.sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
-}
-
-// Objects
-class Object {
-  constructor(x, y, radius, color) {
-    this.x = x;
-    this.y = y;
-    this.radius = radius;
-    this.color = color;
-  }
-
-  draw() {
-    c.beginPath();
-    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
-    // c.fillStyle = this.color;
-    // c.fill();
-    c.strokeStyle = this.color;
-    c.stroke();
-    c.closePath();
-  }
-
-  update() {
-    this.draw();
-  }
-}
-
-function rotate(velocity, angle) {
-  const rotatedVelocities = {
-    x: velocity.x * Math.cos(angle) - velocity.y * Math.sin(angle),
-    y: velocity.x * Math.sin(angle) + velocity.y * Math.cos(angle),
-  };
-
-  return rotatedVelocities;
-}
-
-function resolveCollision(particle, otherParticle) {
-  const xVelocityDiff = particle.velocity.x - otherParticle.velocity.x;
-  const yVelocityDiff = particle.velocity.y - otherParticle.velocity.y;
-
-  const xDist = otherParticle.x - particle.x;
-  const yDist = otherParticle.y - particle.y;
-
-  // Prevent accidental overlap of particles
-  if (xVelocityDiff * xDist + yVelocityDiff * yDist >= 0) {
-    // Grab angle between the two colliding particles
-    const angle = -Math.atan2(
-      otherParticle.y - particle.y,
-      otherParticle.x - particle.x
-    );
-
-    // Store mass in var for better readability in collision equation
-    const m1 = particle.mass;
-    const m2 = otherParticle.mass;
-
-    // Velocity before equation
-    const u1 = rotate(particle.velocity, angle);
-    const u2 = rotate(otherParticle.velocity, angle);
-
-    // Velocity after 1d collision equation
-    const v1 = {
-      x: (u1.x * (m1 - m2)) / (m1 + m2) + (u2.x * 2 * m2) / (m1 + m2),
-      y: u1.y,
-    };
-    const v2 = {
-      x: (u2.x * (m1 - m2)) / (m1 + m2) + (u1.x * 2 * m2) / (m1 + m2),
-      y: u2.y,
-    };
-
-    // Final velocity after rotating axis back to original location
-    const vFinal1 = rotate(v1, -angle);
-    const vFinal2 = rotate(v2, -angle);
-
-    // Swap particle velocities for realistic bounce effect
-    particle.velocity.x = vFinal1.x;
-    particle.velocity.y = vFinal1.y;
-
-    otherParticle.velocity.x = vFinal2.x;
-    otherParticle.velocity.y = vFinal2.y;
-  }
-}
-
-let particles = [];
-
-function init() {
+let increment = wave.frequency;
+function draw() {
   c.beginPath();
+
   c.moveTo(0, canvas.height / 2);
   for (let i = 0; i < canvas.width; i++) {
-    c.lineTo(canvas.height + i, Math.sqrt(canvas.height + i));
+    c.lineTo(
+      i,
+      wave.y +
+        Math.sin(i * wave.length + increment) *
+          wave.amplitude *
+          Math.sin(increment)
+    );
   }
+  c.strokeStyle = `rgb(${Math.round(
+    colorControl.r / 2 + (colorControl.r / 2) * Math.sin(increment)
+  )}, ${colorControl.g}, ${colorControl.b})`;
   c.stroke();
 }
 
-// Animation Loop
 function animate() {
   requestAnimationFrame(animate);
-  c.clearRect(0, 0, canvas.width, canvas.height);
-
+  // c.clearRect(0, 0, canvas.width, canvas.height);
+  c.fillStyle = "rgba(0,0,0,0.01)";
+  c.fillRect(0, 0, canvas.width, canvas.height);
   // c.fillText("HTML CANVAS BOILERPLATE", mouse.x, mouse.y);
-
-  //Make drag
-  // particle.x = mouse.x;
-  // particle.y = mouse.y;
-  particles.forEach((particle) => {
-    particle.update(particles);
-  });
+  draw();
+  increment += wave.frequency;
 }
 
 init();
-// animate();
+animate();
